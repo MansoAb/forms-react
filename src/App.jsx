cfb6aafb1a615842cbabc706592b7b0e468da32e
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-11-03 03:54:32 +0300

    mans

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./index.css";
+
+const App = (props) => {
+  const [text, setText] = useState("");
+  const handleText = (e) => {
+    setText(e.target.value);
+    setBl(text.length < 1 ? true : null);
+  };
+  const [bl, setBl] = useState(true);
+
+  const handleM = (e) => {
+    if (text.length > 0) {
+      console.log(text);
+      setText("");
+      setBl(false);
+    }
+  };
+
+  return (
+    <div>
+      {" "}
+      <div className={`block ${!bl ? "green" : null}`}>
+        <div className="block1">
+          <input
+            className={`inputText ${bl ? "is-error" : null}`}
+            value={text}
+            onChange={handleText}
+          ></input>
+          <input
+            type="button"
+            className={`button ${text.length < 1 ? "deactiv" : null}`}
+            value="Отправить"
+            onClick={handleM}
+          />
+        </div>
+        <div>
+          {bl
+            ? "Поле ввода не должно быть пустым."
+            : bl === false
+            ? "Сообшение отправлено"
+            : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
